ddb031ea7667670363dbb1cb02f8c084774913c7
michaelwstuart <mike.w.stuart@gmail.com>
2017-09-06 03:43:21 -0700

    found the broken code

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import {} from 'dotenv/config';
 import Yobit from 'yobit';
 
+// a little patch
+Yobit.prototype.addTrade = function addTrade(callback, pair, type, rate, amount) {
+  this.privateRequest('Trade', { pair, type, rate, amount }, callback)
+}
+
 const publicClient = new Yobit();
 const privateClient = new Yobit(process.env.API_KEY, process.env.API_SECRET);
 
@@ -9,21 +14,15 @@ const INTERVAL = 1000 * 3;
 const SELL_PRICE = 0.00009;
 const BUY_PRICE = 0.00007;
 
-let market = 'sell_market';
+let mode = 'sell';
 
-const trade = (quantity, currentPrice) => {
-  let mode;
-  yobit.addTrade((err, data) => {
+const trade = (currentPrice, quantity) => {
+  privateClient.addTrade((err, data) => {
     if (err) throw new Error(err);
-    if (market === 'sell_market') {
-      mode = 'SOLD';
-      market = 'buy_market';
-    } else {
-      mode = 'PURCHASED';
-      market = 'sell_market'
-    }
+    if (!data.success) throw new Error(data.error);
+    mode = mode === 'sell' ? 'buy' : 'sell';
     console.log(`${mode} ${quantity} on ${new Date()}`);
-  }, MY_COIN, market, quantity, currentPrice);
+  }, MY_COIN, mode, currentPrice, quantity);
 }
 
 const examineFunds = callback =>
@@ -34,18 +33,26 @@ const examineFunds = callback =>
 
 const makeMoney = currentPrice => {
   let quantity;
-  if ((market === 'sell_market') && (currentPrice > SELL_PRICE))
+  if ((market === 'sell') && (currentPrice > SELL_PRICE))
     examineFunds(funds => quantity = funds.nlc2);
-  if ((market === 'buy_market') && (currentPrice < BUY_PRICE))
+  if ((market === 'buy') && (currentPrice < BUY_PRICE))
     examineFunds(funds => quantity = (funds.btc / currentPrice) - .1);
   if (quantity)
     trade(quantity, currentPrice);
 }
 
 setInterval(() => {
-  console.log('*tick*')
   publicClient.getTicker((err, coin) => {
     if (err) throw new Error(err);
-    else makeMoney(coin[MY_COIN].last);
+    if (!data.success) throw new Error(data.error);
+    makeMoney(coin.nlc2_btc.last);
   }, MY_COIN);
 }, INTERVAL);
+
+// let mode;
+// publicClient.getTicker(MY_COIN, (err1, coin) => {
+//   console.log(coin)
+//   privateClient.addTrade(MY_COIN, 'sell', coin.nlc2_btc.last, 13, (err2, data) => {
+//     console.log(data)
+//   });
+// })
