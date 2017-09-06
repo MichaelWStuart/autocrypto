f909ea10db52bff6a62a6d1425c627f4a026f009
michaelwstuart <mike.w.stuart@gmail.com>
2017-09-06 03:49:40 -0700

    forgot to save

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const publicClient = new Yobit();
 const privateClient = new Yobit(process.env.API_KEY, process.env.API_SECRET);
 
 const MY_COIN = 'nlc2_btc';
-const INTERVAL = 1000 * 3;
+const INTERVAL = 10000;
 const SELL_PRICE = 0.00009;
 const BUY_PRICE = 0.00007;
 
@@ -18,7 +18,6 @@ let mode = 'sell';
 
 const trade = (currentPrice, quantity) => {
   privateClient.addTrade((err, data) => {
-    if (err) throw new Error(err);
     if (!data.success) throw new Error(data.error);
     mode = mode === 'sell' ? 'buy' : 'sell';
     console.log(`${mode} ${quantity} on ${new Date()}`);
@@ -27,32 +26,23 @@ const trade = (currentPrice, quantity) => {
 
 const examineFunds = callback =>
   privateClient.getInfo((err, data) => {
-    if (err) throw new Error(err);
+    if (!data.success) throw new Error(data.error);
     callback(data.return.funds)
   })
 
 const makeMoney = currentPrice => {
   let quantity;
-  if ((market === 'sell') && (currentPrice > SELL_PRICE))
+  if ((mode === 'sell') && (currentPrice > SELL_PRICE))
     examineFunds(funds => quantity = funds.nlc2);
-  if ((market === 'buy') && (currentPrice < BUY_PRICE))
+  if ((mode === 'buy') && (currentPrice < BUY_PRICE))
     examineFunds(funds => quantity = (funds.btc / currentPrice) - .1);
   if (quantity)
-    trade(quantity, currentPrice);
+    trade(currentPrice, quantity);
 }
 
 setInterval(() => {
   publicClient.getTicker((err, coin) => {
-    if (err) throw new Error(err);
     if (!data.success) throw new Error(data.error);
     makeMoney(coin.nlc2_btc.last);
   }, MY_COIN);
 }, INTERVAL);
-
-// let mode;
-// publicClient.getTicker(MY_COIN, (err1, coin) => {
-//   console.log(coin)
-//   privateClient.addTrade(MY_COIN, 'sell', coin.nlc2_btc.last, 13, (err2, data) => {
-//     console.log(data)
-//   });
-// })
